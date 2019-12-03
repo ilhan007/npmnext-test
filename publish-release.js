6572c141b700f0a95207d349560ecf540c77c09e
ilhan <ilhan.myumyun@sap.com>
2019-12-03 06:49:11 +0200

    update action

---
+++ publish-release.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+const path = require('path');
+const { execSync } = require('child_process');
+
+let pkg = require(path.join(process.cwd(), 'package.json'))
+
+const run = async () => {
+	if (!process.env.NPM_AUTH_TOKEN) {
+		throw new Error('Merge-release requires NPM_AUTH_TOKEN');
+	}
+
+  let current = execSync(`npm view ${pkg.name} version`).toString()
+  exec(`npm version --allow-same-version=true --git-tag-version=false ${current} `)
+  console.log('current:', current, '/', 'version:', version);
+	console.log('new version:', newVersion)
+}
+run();
