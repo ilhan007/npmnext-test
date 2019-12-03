06aab2c7455b11bb5226f47a5d22123827ea68b6
ilhan <ilhan.myumyun@sap.com>
2019-12-03 08:04:50 +0200

    turn to mono repo

---
+++ packages/main/src/Field.js
@@ -0,0 +1,27 @@
+/**
+* Field webcomponent, wrapping a native input.
+* @since 1.0.0
+* @class
+* @public
+* @tagname my-field
+*/
+class Field extends HTMLElement {
+	constructor() {
+		super();
+
+		const shadow = this.attachShadow({ mode: "open" });
+		const root = document.createElement('div');
+		const input = document.createElement('input');
+		input.setAttribute("type", "text");
+		input.setAttribute("placeholder", "Hello, this is a text field");
+		const style = document.createElement('style');
+
+		shadow.appendChild(style);
+		root.appendChild(input)
+		shadow.appendChild(root);
+	}
+}
+
+window.customElements.define("my-field", Field);
+
+export default Field;
