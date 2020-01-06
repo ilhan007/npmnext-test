d773963b8a2354194c3eaebee53a79b18838a8bb
ilhan <ilhan.myumyun@sap.com>
2020-01-06 14:19:29 +0200

    fix: version generation

--- .github/actions/pre-release/index.js
+++ .github/actions/pre-release/index.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const { promisify } = require("util");
 const readFileAsync = promisify(fs.readFile);
 const writeFileAsync = promisify(fs.writeFile);
-const semver = require("semver");
 const child_process = require("child_process");
 const glob = require("glob-promise");
 const gitRev = child_process.execSync("git rev-parse HEAD").toString();
@@ -16,10 +15,14 @@ const run = async () => {
 		const packageJSONFile = await readFileAsync(file);
 		const pkgJSON = JSON.parse(packageJSONFile.toString());
 
-		const rc = pkgJSON.version.toString().includes("rc") ? "-rc": "";
-		pkgJSON.version = `${pkgJSON.version}${rc}.${gitRev.slice(0,7,)}`;
+		// Checks if the current version already has "RC"(1.0.0-rc.5) or not (0.18.0)
+		// The version with "RC" would become 1.0.0-rc.{RC_NUMBER}.{HASH}
+		// The version without "RC" would become 0.18.0-dev.{HASH}
+		const tag = pkgJSON.version.toString().includes("rc") ? "" : "-dev"
 
+		pkgJSON.version = `${pkgJSON.version}${tag}.${gitRev.slice(0,7,)}`;
 		console.log("Prerelease version: " + pkgJSON.version);
+
 		await writeFileAsync(file, JSON.stringify(pkgJSON, null, "  "));
 
 		return exec(`npm publish ${package} --tag=next`);
@@ -30,4 +33,4 @@ const run = async () => {
 
 run().catch(error => {
 	console.log("action failed", error);
-})
+});
