004b91bc485e28fe89a9d2502ef67fa03c779bc3
ilhan <ilhan.myumyun@sap.com>
2020-01-06 16:58:19 +0200

    fix: update deps

--- .github/actions/pre-release/index.js
+++ .github/actions/pre-release/index.js
@@ -10,7 +10,7 @@ const { exec } = require("@actions/exec");
 // {name, version, pkgFile}
 const run = async () => {
 	const packages = {};
-	const files = await glob("**/packages/**/package.json", { "ignore": "**/node_modules/**/*.*" });
+	const pkgJSONFiles = await glob("**/packages/**/package.json", { "ignore": "**/node_modules/**/*.*" });
 
 	const generateNewVersions = async (package, pkgJSONContent) => {
 		// Checks if the current version already has "-rc" (1.0.0-rc.5) or not (0.18.0).
@@ -20,11 +20,11 @@ const run = async () => {
 		const suffix = currentVersion.toString().includes("rc") ? "" : "-dev";
 		const newVersion = `${currentVersion}${suffix}.${gitRev.slice(0,7,)}`;
 
-		packages[`@${package}`] = newVersion;
-		console.log(`@${package} next version: ${newVersion}`);
+		packages[pkgJSONContent.name] = newVersion;
+		console.log(`${pkgJSONContent.name} next version: ${newVersion}`);
 	}
 
-	const updatePackageJSON = async (package, packageJSONFile, pkgJSONContent) => {
+	const updatePackageJSON = async (package, pkgJSONFile, pkgJSONContent) => {
 		pkgJSONContent.version = packages[package];
 
 		const dependencies = pkgJSONContent.dependencies;
@@ -36,28 +36,31 @@ const run = async () => {
 				console.log(`updated dependency: ${dep} to ${pkgJSONContent.dependencies[dep]}`);
 			});
 		}
-		await writeFileAsync(packageJSONFile, JSON.stringify(pkgJSONContent, null, "  "));
+		await writeFileAsync(pkgJSONFile, JSON.stringify(pkgJSONContent, null, "  "));
 	}
 
-	// generate new npm versions
-	await Promise.all(files.map(async (file) => {
-		const package = file.split("package.json")[0];
-		const packageJSONFile = await readFileAsync(file);
-		const pkgJSONContent = JSON.parse(packageJSONFile.toString());
+	// Step 1: store packages info
+	await Promise.all(pkgJSONFiles.map(async (file) => {
+		const pkgJSONFile = await readFileAsync(file);
+		const pkgJSONContent = JSON.parse(pkgJSONFile.toString());
+		const pkgName = pkgJSONContent.name;
 
-		await generateNewVersions(package, pkgJSONContent);
+		packages[pkgName] = {
+			pkgName,
+			pkgJSONFile,
+			pkgJSONContent
+		};
+
+		console.log("Packages", packages);
 	}));
 
-	// update package dependencies versions
-	await Promise.all(files.map(async (file) => {
-		const package = file.split("package.json")[0];
-		const packageJSONFile = await readFileAsync(file);
-		const pkgJSONContent = JSON.parse(packageJSONFile.toString());
-
-		await updatePackageJSON(package, packageJSONFile, pkgJSONContent);
+	// Step 2: generate new npm versions and update package dependencies versions
+	await Promise.all(Object.keys(packages).map(async pkg => {
+		await generateNewVersions(pkg.pkgName, pkg.pkgJSONContent);
+		await updatePackageJSON(pkg.pkgName, pkg.pkgJSONFile, pkg.pkgJSONContent);
 	}));
 
-	// Publish all packages to npm
+	// Step 3: Publish all packages to npm
 	await Promise.all(Object.keys(packages).map(pkg => exec(`npm publish ${pkg} --tag=next`)));
 }
 
