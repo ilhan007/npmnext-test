90192893423c47f2bbbf37fe38f07a7c961caf5e
ilhan <ilhan.myumyun@sap.com>
2020-01-07 11:39:52 +0200

    chore(prerelease): test auth

--- .github/actions/pre-release/setup-npm.sh
+++ .github/actions/pre-release/setup-npm.sh
@@ -11,9 +11,9 @@ echo "//registry.npmjs.org/:username=${NPM_USERNAME}" >> ./packages/base/.npmrc
 echo "//registry.npmjs.org/:email=${NPM_EMAIL}" >> ./packages/base/.npmrc
 echo "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" >> ./packages/base/.npmrc
 
-echo "//registry.npmjs.org/:username=${NPM_USERNAME}" >> ./packages/min/.npmrc
-echo "//registry.npmjs.org/:email=${NPM_EMAIL}" >> ./packages/min/.npmrc
-echo "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" >> ./packages/min/.npmrc
+echo "//registry.npmjs.org/:username=${NPM_USERNAME}" >> ./packages/main/.npmrc
+echo "//registry.npmjs.org/:email=${NPM_EMAIL}" >> ./packages/main/.npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" >> ./packages/main/.npmrc
 
 echo "//registry.npmjs.org/:username=${NPM_USERNAME}" >> ./packages/fiori/.npmrc
 echo "//registry.npmjs.org/:email=${NPM_EMAIL}" >> ./packages/fiori/.npmrc
