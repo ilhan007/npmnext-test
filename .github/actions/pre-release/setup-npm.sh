bcafe9841a5b41848c57f402091f90b2e494e783
ilhan <ilhan.myumyun@sap.com>
2020-01-07 11:35:27 +0200

    chore(prerelease): test auth

--- .github/actions/pre-release/setup-npm.sh
+++ .github/actions/pre-release/setup-npm.sh
@@ -6,5 +6,3 @@ set -o errexit
 echo "//registry.npmjs.org/:username=${NPM_USERNAME}" >> ./.npmrc
 echo "//registry.npmjs.org/:email=${NPM_EMAIL}" >> ./.npmrc
 echo "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" >> ./.npmrc
-npm adduser
-npm whoami
