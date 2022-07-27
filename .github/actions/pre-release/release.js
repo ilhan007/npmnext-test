960c2005647299125268e4a74e366ed93934f7e3
ilhan007 <ilhan.myumyun@sap.com>
2022-07-27 21:04:59 +0300

    ci: update worflow

--- .github/actions/pre-release/release.js
+++ .github/actions/pre-release/release.js
@@ -1,17 +1,26 @@
-const fs = require("fs");
-const { promisify } = require("util");
-const readFileAsync = promisify(fs.readFile);
-const writeFileAsync = promisify(fs.writeFile);
+const fs = require("fs").promises;
 const child_process = require("child_process");
-const glob = require("glob-promise");
+const commandLineArgs = require('command-line-args');
 const execSync = child_process.execSync;
 const gitRev = execSync("git rev-parse HEAD").toString();
 
 const PACKAGES = {};
 const NPM_ORG = "@next-level";
 
+const options = commandLineArgs([
+	{ name: 'version', alias: 'v', type: String },
+	{ name: 'tag', alias: 't', type: String },
+	{ name: 'otp', alias: 'p', type: String },
+]);
+
+const DEFAULT_TAG = "next";
+const TAG = options.tag || DEFAULT_TAG;
+const NEW_VERSION = options.version;
+const OTP = options.otp;
+
 const run = async () => {
-	const FILES = await glob("**/packages/**/package.json", { "ignore": "**/node_modules/**/*.*" });
+	const { globby } = await import("globby");
+	let FILES = await globby(["packages/*/package.json"]);
 
 	// Step 1: process package.json files
 	const pkgs = await Promise.all(FILES.map(processPackageJSON));
@@ -25,11 +34,11 @@ const run = async () => {
 
 const processPackageJSON = async file => {
 	const folder = file.split("package.json")[0];
-	const fileRead = await readFileAsync(file);
+	const fileRead = await fs.readFile(file);
 	const fileContent = JSON.parse(fileRead.toString());
 	const name = fileContent.name;
 
-	const version = `0.0.0-${gitRev.slice(0,9,)}`;
+	const version = NEW_VERSION || `0.0.0-${gitRev.slice(0,9,)}`;
 
 	PACKAGES[name] = { name, file, fileContent, version, folder };
 	return PACKAGES[name];
@@ -49,7 +58,7 @@ const updatePackageJSON = async pkg => {
 		fileContent.devDependencies[dep] = PACKAGES[dep].version;
 	});
 
-	return writeFileAsync(file, JSON.stringify(fileContent, null, "  "));
+	return fs.writeFile(file, JSON.stringify(fileContent, null, "  "));
 };
 
 const getDependencies = (dependencies) => {
@@ -57,10 +66,11 @@ const getDependencies = (dependencies) => {
 };
 
 const publishPackage = pkg => {
-	console.info(`Publish ${pkg.name}: ${pkg.version} ...`);
-	execSync(`yarn publish ${pkg.folder} --tag=next`);
+	console.info(`Publish ${pkg.name}: ${pkg.version} ...`); // eslint-disable-line
+	const OTP_PARAM = OTP ? `--otp=${OTP}` : ``;
+	execSync(`yarn publish ${pkg.folder} --tag=${TAG} --new-version=${pkg.version} ${OTP_PARAM}`);
 };
 
 run().catch(error => {
-	console.error("Release of @next version failed", error);
+	console.error("Release of @next version failed", error); // eslint-disable-line
 });
